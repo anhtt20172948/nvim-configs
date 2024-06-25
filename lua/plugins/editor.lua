f7b058284e33d9d417664fcfe483b9b22d8aa495
anhtran <uiui1999vn@gmail.com>
2024-06-25 15:53:06 +0700

    add git reset hunk

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -237,6 +237,7 @@ return {
             require("gitsigns").setup()
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+            vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
         end
     },
     -- }}}
@@ -256,4 +257,5 @@ return {
             -- ...
         end
     } -- }}}
+
 }
